49479fd29d63fa1597a02b6279965176596f74f6
delve <delve202@gmail.com>
2014-07-21 22:55:17 -0400

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -153,7 +153,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   var tileIMG = document.createElement("img");
   var tileURL = document.createElement("a");
 //  var randomWiki = fetchRandom();
-  var randomWiki = new WikipediaCORS();
+  var randomWiki = new WikipediaCORS.init();
 
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
